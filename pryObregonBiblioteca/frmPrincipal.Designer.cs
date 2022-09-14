72c9d6673be4ff43a8ece40c64bfea365f2fe1b6
root <mgiraudo@ies21.edu.ar>
2022-09-14 14:00:21 -0300

    Agregar archivos de proyecto.

---
+++ pryObregonBiblioteca/frmPrincipal.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace pryObregonBiblioteca
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.BackColor = System.Drawing.Color.Transparent;
+            this.lblBienvenido.Font = new System.Drawing.Font("Old English Text MT", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenido.ForeColor = System.Drawing.Color.Black;
+            this.lblBienvenido.Location = new System.Drawing.Point(40, 139);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(478, 40);
+            this.lblBienvenido.TabIndex = 0;
+            this.lblBienvenido.Text = "¡Bienvenido a la BiblioVirtual!";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::pryObregonBiblioteca.Properties.Resources.gatito_pasando_pagina;
+            this.ClientSize = new System.Drawing.Size(551, 213);
+            this.Controls.Add(this.lblBienvenido);
+            this.Name = "frmPrincipal";
+            this.Text = "BiblioVirtual";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenido;
+    }
+}
+
